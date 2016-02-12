31fce14314df1aa1e6cd08c397c1f0ae9c404467
Jeffery Wise <jrwise@gmail.com>
2016-02-11 18:50:15 -0600

    Updated controller assignments

--- src/org/usfirst/frc/team3946/robot/OI.java
+++ src/org/usfirst/frc/team3946/robot/OI.java
@@ -1,5 +1,13 @@
 package org.usfirst.frc.team3946.robot;
 
+import org.usfirst.frc.team3946.robot.commands.Launch;
+import org.usfirst.frc.team3946.robot.commands.LowerIntake;
+import org.usfirst.frc.team3946.robot.commands.PosCatForLaunch;
+import org.usfirst.frc.team3946.robot.commands.PosCatForLoad;
+import org.usfirst.frc.team3946.robot.commands.RaiseIntake;
+
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import libraries.XboxController;
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -11,6 +19,21 @@ public class OI {
     
     public OI() {
     	driveController.setDeadband(0.2);
+    	
+    //Catapult
+    	Button launch = new JoystickButton(driveController, XboxController.RightBumper);
+    	Button posCatForLaunch = new JoystickButton(driveController, XboxController.B);
+    	Button posCatForLoad = new JoystickButton(driveController, XboxController.A);
+    		launch.whileActive(new Launch());
+    		posCatForLaunch.whileActive(new PosCatForLaunch());
+    		posCatForLoad.whileActive(new PosCatForLoad());
+    		
+    //Intake
+    	Button lowerIntake = new JoystickButton(driveController, XboxController.X);
+    	Button raiseIntake = new JoystickButton(driveController, XboxController.Y);
+    		lowerIntake.whileActive(new LowerIntake());
+    		raiseIntake.whileActive(new RaiseIntake());
+    	
     }
 }
 
